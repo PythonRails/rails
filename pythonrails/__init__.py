abcafba8e063638c261d81f5f54d5f687ed7f84a
Anton Danilchenko <anton.danilchenko@gmail.com>
2015-07-31 21:34:44 +0300

    Added project_dir option

--- pythonrails/__init__.py
+++ pythonrails/__init__.py
@@ -3,11 +3,11 @@ from wsgiref.simple_server import make_server
 from pythonrails.router import Router
 
 
-def run(host='127.0.0.1', port=8000):
+def run(host='127.0.0.1', port=8000, project_dir=None):
     """
     Run web server.
     """
     print "Server running on {}:{}".format(host, port)
-    app_router = Router()
+    app_router = Router(project_dir=project_dir)
     server = make_server(host, port, app_router)
     server.serve_forever()
