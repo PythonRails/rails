dcceaea3777354efbffb6be606722146493ebfd9
Anton Danilchenko <anton.danilchenko@gmail.com>
2015-08-09 16:08:26 +0300

    Added some docs inside the test 'blog' project

--- test_projects/blog/controllers/index.py
+++ test_projects/blog/controllers/index.py
@@ -2,9 +2,22 @@ from pythonrails.response import Response
 
 
 class Index(object):
+    """
+    Index controller is a root project controller.
+
+    With this controller you can show welcome page and handle all non-existing pages.
+    """
 
     def index(self, request):
+        """
+        Show welcome page of the project.
+        """
         return Response("Website home page")
 
     def not_found(self, request):
+        """
+        Handle all pages that doesn't have an action inside this controller.
+
+        For example, you can open page '/index/something' - to go to this method.
+        """
         return Response("Handle all pages near the Index controller. Now requested {}".format(request.path))
