b035651a783855c0cb02e80b76f31ca2bf12743f
Anton Danilchenko <anton.danilchenko@gmail.com>
2015-11-19 01:39:59 +0200

    Move test project in external repository

--- test_projects/blog/app.py
+++
@@ -1,5 +0,0 @@
-from pythonrails import run
-
-
-if __name__ == '__main__':
-    run(port=8500)
--- test_projects/blog/controllers/blog_articles.py
+++
@@ -1,7 +0,0 @@
-from pythonrails.response import Response
-
-
-class BlogArticles(object):
-
-    def index(self, request):
-        return Response('Blog articles home page')
