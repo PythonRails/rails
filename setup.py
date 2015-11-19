dc883dd4288eca58091febecee118b630b1b4479
Anton Danilchenko <anton.danilchenko@gmail.com>
2015-11-19 02:42:20 +0200

    Change texts

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 Rails
 ====================
 
-**Rails on Python** is a web framework with an idea to simplify web development.
+**Python on Rails** is a web framework with an idea to simplify web development.
 It's not a clone of **Ruby on Rails**. This project created for for less code,
 and this code should be good structured in small and big projects.
 
